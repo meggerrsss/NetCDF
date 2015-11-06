c68d93b14d3ba787d75a475cccac1c10a6f7142a
Meggerrsss <meghanleighgreen@gmail.com>
2015-11-06 13:33:07 -0400

    netcdfread
    from Sean mostly, some edits like unite and nomlonge in order to quicker
    get the units and full title of what's going on, probably for axes later

---
+++ netcdfread.py
@@ -0,0 +1,45 @@
+__author__ = 'Meghan'
+
+from netCDF4 import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+
+nom = 'pamtest_RC2_pam.nc' ###############
+
+# open file nom='filename.nc' in read mode and store in variable c
+c = Dataset( 'C:\Users\Meghan\Desktop\MSC1\Research\Output files\{0}'.format(nom), 'r', format='NETCDF4')
+
+# find out what variables are in c
+print c.variables
+
+# print variable header information for 'v'
+
+v = 'raddry' ##########
+print c.variables[v]
+
+# store 'v' value in x
+x = c.variables[v][:]
+
+# units of v
+unite = c.variables[v].__dict__['units']
+# long_name of v
+nomlonge = c.variables[v].__dict__['long_name']
+
+
+temps = c.variables['time'][:]
+altitude = c.variables['level'][:]
+
+
+# plot x
+plt.plot(x)
+
+
+# from http://matplotlib.org/users/pyplot_tutorial.html
+#plt.xlabel('x-label')
+#plt.ylabel(nomlonge) ##still not totally sure this is true
+#plt.title('Histogram of IQ')
+#plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+#plt.axis([40, 160, 0, 0.03])
+#plt.grid(True)
+#plt.show()
+
